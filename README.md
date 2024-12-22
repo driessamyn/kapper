726e42f01bd1d83b88b06d5e2a03111a41ed68fa
Dries Samyn <5557551+driessamyn@users.noreply.github.com>
2024-12-22 22:14:27 +0100

    chore: documentation improvements
    - API doc improvements
    - Updated README.md
    - Added CONTRIBUTING.md and CODE_OF_CONDUCT.md

--- README.md
+++ README.md
@@ -11,43 +11,116 @@ The main goals of the Kapper ORM are:
 2. **Performance**: Minimize overhead and dependencies to ensure fast execution of queries and updates.
 3. **Flexibility**: Allow integration with various database drivers and connection management strategies.
 
+Kapper aims to go against the grain of the heavy weight database abstractions such as Hibernate and JOOQ.
+Instead, it makes JDBC support easy without taking away any flexibility.
+In fact, it can happily live alongside an existing _vanilla_ JDBC integration.
+It does not generate code, it does not introduce another layer of abstraction, it is never intrusive.
+
+Kapper also means hairdresser in Dutch, but I have yet to come up with a reason for why this is relevant.
+
 ## Features
 
 - **Simple API**: Kapper provides a familiar set of methods for executing SQL queries, mapping results to Kotlin data classes, and updating/inserting data.
-- **Dynamic Parameters**: The `DynamicParameters` class makes it easy to bind parameters to SQL statements.
 - **Extensibility**: The Kapper API is implemented as extension functions on the `java.sql.Connection` interface, allowing seamless integration with existing JDBC code.
 - **Lightweight**: Kapper has minimal external dependencies, focusing on providing core functionality without bloat.
 
-> Work on Kapper has only recently started and it is not yet functional.
-> Documentation below covers intended API, not final.
+Kapper is in early development phase and while it is functional, breaking API changes may be made until its first stable release.
+
+Snapshot releases are published from the main branch to [GitHub Packages](packages/2353016).
+Stable releases will be published to Maven Central.
 
 ## Usage
 
 Here's a simple example of how to use Kapper:
 
+Example DB table
+```sql
+ CREATE TABLE super_heroes (
+    id UUID PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    age INT
+);
+ ```
+
+and an example DTO:
 ```kotlin
-// Assuming you have a java.sql.Connection instance
-val connection: Connection = ...
-
-// Execute a SQL query and map the results to a list of User objects
-val users: List<User> = connection.query<User>("SELECT * FROM users")
-
-// Execute a SQL statement with parameters
-val updatedRows = connection.execute(
-    "UPDATE users SET name = :name WHERE id = :id",
-    name" to "John Doe", "id" to 123
-)
-
-// Query a single result and map it to a User object
-val user: User? = connection.querySingle<User>(
-    "SELECT * FROM users WHERE id = :id",
-    "id" to 1
-)
+data class SuperHero(val id: UUID, val name: String, val email: String? = null, val age: Int? = null)
 ```
+
+You can use Kapper like so:
+```kotlin
+// Assuming you have a java.sql.Connection instance, using your favourite connection pooler, for example:
+ds.getConnection().use { connection ->
+    // insert a row:
+    connection.execute(
+        "INSERT INTO super_heroes(id, name, email, age) VALUES(:id, :name, :email, :age);",
+        "id" to UUID.randomUUID(),
+        "name" to "Batman",
+        "email" to "batman@dc.com",
+        "age" to 85,
+    )
+    
+    // Execute a SQL query and map the results to a list of SuperHero objects
+    val heroes: List<SuperHero> = connection.query<SuperHero>("SELECT * FROM super_heroes")
+
+    // or query by passing parameters
+    val olderHeroes = connection.query<SuperHero>("SELECT * FROM super_heroes WHERE age > :age", "age" to 80)
+    
+    // or find a single
+    val batman = connection.querySingle<SuperHero>(
+        "SELECT id, name FROM super_heroes WHERE name = :name",
+        "name" to "Batman",
+    )
+}
+```
+
+Other examples are available in the [integration tests](lib/src/integrationTest/kotlin/net/samyn/kapper/).
+
+Kapper does not maintain a mapping between classes and DB tables or entities. 
+Instead, it can either map to a given class if the constructor arguments match the DB fields, or a mapping lambda can be passed in.
+This means Kapper provides a strongly typed mapping, but still allows rows-to-object mapping with lambdas for more flexibility or advanced used cases.
+
+## Using Kapper in your project
+
+Currently, only snapshot releases are published.
+Stable versions will be published in due course, but if you want to try the snapshot releases, add the following dependency to your `build.gradle.kts`:
+
+  ```kotlin
+   dependencies {
+       implementation("com.github.driessamyn.kapper:kapper:<version>")
+   }
+   ```
+Or for Maven:
+   ```xml
+   <dependency>
+       <groupId>com.github.driessamyn.kapper</groupId>
+       <artifactId>kapper</artifactId>
+       <version>{version}</version>
+   </dependency>
+   ```
+
+## Database support
+
+Kapper's integration tests currently cover Postgresql and MySQL.
+Other DBs will be added.
+If you need support for another DB, and/or find an issue with a particular DB, feel free to [open an issue](kapper/issues) or, even better, submit a pull request. 
+
+## Roadmap
+
+Kapper is in its early stages of development.
+The following will be worked on in the next few releases:
+
+- Introduce SLF4J for logging. 
+- Example repo to enhance the documentation.
+- Create a benchmark suite to validate performance.
+- Cache query parsing.
+- Custom SQL type conversion.
+- Add MS SQL Server and Oracle integration tests.
 
 ## Contributing
 
-We welcome contributions to the Kapper ORM project! If you find any issues or have ideas for improvements, please feel free to open an issue or submit a pull request.
+We welcome contributions to the Kapper! If you find any issues or have ideas for improvements, please feel free to [open an issue](kapper/issues) or submit a pull request.
 
 ## License
 
--- lib/src/main/kotlin/net/samyn/kapper/Kapper.kt
+++ lib/src/main/kotlin/net/samyn/kapper/Kapper.kt
@@ -13,9 +13,32 @@ val impl: Kapper by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
 /**
  * Execute a SQL query and map the results to a list of instances of the specified class.
  *
+ * This function uses reflection to automatically map the result set columns to the properties of the specified class.
+ * For advanced mappings, use the overloaded version with a custom `mapper` function.
+ *
+ * **Example**:
+ * ```kotlin
+ * // Assuming we have a data class
+ * data class User(val id: Int, val name: String)
+ *
+ * // Fetch all users where "active" is true
+ * val users: List<User> = connection.query(
+ *     sql = "SELECT id, name FROM users WHERE active = :active",
+ *     "active" to true
+ * )
+ *
+ * // Iterate over the user list and print each user
+ * users.forEach { println(it) }
+ * ```
+ *
+ * **Edge Cases**:
+ * - Returns an empty list if the query yields no results.
+ * - Throws an exception if the query is invalid or if there's an issue with the database connection.
+ *
  * @param sql The SQL query to execute.
- * @param args Optional parameters to be substituted in the SQL query.
+ * @param args Optional key-value pairs representing named parameters to substitute into the query.
  * @return The query result as a list of [T] instances.
+ * @throws java.sql.SQLException If there's a database error.
  */
 inline fun <reified T : Any> Connection.query(
     sql: String,
@@ -25,11 +48,25 @@ inline fun <reified T : Any> Connection.query(
 }
 
 /**
- * Execute a SQL query and map the results to a list of instances of the specified class.
+ * Execute a SQL query and map the results to a list of instances of the specified class with a custom mapper.
+ *
+ * **Example**:
+ * ```kotlin
+ * val users: List<User> = connection.query(
+ *     sql = "SELECT id, name FROM users",
+ *     mapper = { resultSet, _ ->
+ *         User(
+ *             id = resultSet.getInt("id"),
+ *             name = resultSet.getString("name")
+ *         )
+ *     }
+ * )
+ * users.forEach { println(it) }
+ * ```
  *
  * @param sql The SQL query to execute.
- * @param mapper Optional mapping function to map the [ResultSet] to the target class.
- * @param args Optional parameters to be substituted in the SQL query.
+ * @param mapper Custom mapping function to transform the [ResultSet] into the target class.
+ * @param args Optional parameters to be substituted in the SQL query during execution.
  * @return The query result as a list of [T] instances.
  */
 inline fun <reified T : Any> Connection.query(
@@ -41,9 +78,9 @@ inline fun <reified T : Any> Connection.query(
 /**
  * Execute a SQL query and map the results to a list of instances of the specified class.
  *
- * @param clazz The lass to map the results to.
+ * @param clazz The class to map the results to.
  * @param sql The SQL query to execute.
- * @param args Optional parameters to be substituted in the SQL query.
+ * @param args Optional parameters to be substituted in the SQL query during execution.
  * @return The query result as a list of [T] instances.
  */
 fun <T : Any> Connection.query(
@@ -53,12 +90,12 @@ fun <T : Any> Connection.query(
 ): List<T> = impl.query(clazz.java, this, sql, args.toMap())
 
 /**
- * Execute a SQL query and map the results to a list of instances of the specified class.
+ * Execute a SQL query and map the results to a list of instances of the specified class with a custom mapper.
  *
  * @param clazz The class to map the results to.
  * @param sql The SQL query to execute.
- * @param mapper Optional mapping function to map the [ResultSet] to the target class.
- * @param args Optional parameters to be substituted in the SQL query.
+ * @param mapper Custom mapping function to transform the [ResultSet] into the target class.
+ * @param args Optional parameters to be substituted in the SQL query during execution.
  * @return The query result as a list of [T] instances.
  */
 fun <T : Any> Connection.query(
@@ -69,12 +106,39 @@ fun <T : Any> Connection.query(
 ): List<T> = impl.query(clazz.java, this, sql, mapper, args)
 
 /**
- * Execute a SQL query and map the results to a list of instances of the specified class.
+ * Execute a SQL query and map the result to a single instance of the specified class, or `null` if no results are found.
+ *
+ * This function is useful for queries that are expected to return only one result, such as fetching a specific record by ID.
+ *
+ * **Example**:
+ * ```kotlin
+ * // Assuming we have a data class
+ * data class User(val id: Int, val name: String)
+ *
+ * // Fetch a user by ID
+ * val user: User? = connection.querySingle(
+ *     sql = "SELECT id, name FROM users WHERE id = :id",
+ *     "id" to 1
+ * )
+ *
+ * // Handle the result
+ * if (user == null) {
+ *     println("User not found!")
+ * } else {
+ *     println("Found user: $user")
+ * }
+ * ```
+ *
+ * **Edge Cases**:
+ * - Returns `null` if the query yields no results.
+ * - Throws an exception if the query returns more than one result.
+ * - Throws an exception if the query is invalid or if there's a database connection issue.
  *
  * @param sql The SQL query to execute.
- * @param clazz The class to map the result to.
- * @param args Optional parameters to be substituted in the SQL query.
- * @return The query result single [T] instance or null if 0 results found.
+ * @param args Optional key-value pairs representing named parameters to substitute into the query.
+ * @return A single instance of [T] if exactly one result is found, or `null` otherwise.
+ * @throws java.sql.SQLException If there's a database error.
+ * @throws IllegalStateException If the query yields more than one result.
  */
 inline fun <reified T : Any> Connection.querySingle(
     sql: String,
@@ -84,12 +148,27 @@ inline fun <reified T : Any> Connection.querySingle(
 }
 
 /**
- * Execute a SQL query and map the result to a single instance of the specified class, or null of no results found.
+ * Execute a SQL query and map the result to a single instance of the specified class using a custom mapper or null if no results are found.
+ *
+ * **Example**:
+ * ```kotlin
+ * val customUser: User? = connection.querySingle(
+ *     sql = "SELECT id, name FROM users WHERE id = :id",
+ *     mapper = { resultSet, _ ->
+ *         User(
+ *             id = resultSet.getInt("id"),
+ *             name = resultSet.getString("name")
+ *         )
+ *     },
+ *     "id" to 1
+ * )
+ * println(customUser)
+ * ```
  *
  * @param sql The SQL query to execute.
- * @param mapper Optional mapping function to map the [ResultSet] to the target class.
- * @param args Optional parameters to be substituted in the SQL query.
- * @return The query result single [T] instance or null if 0 results found.
+ * @param mapper Custom mapping function to transform the [ResultSet] into the target class.
+ * @param args Optional parameters to be substituted in the SQL query during execution.
+ * @return The query result as a single instance of [T], or null if no results are found.
  */
 inline fun <reified T : Any> Connection.querySingle(
     sql: String,
@@ -104,8 +183,8 @@ inline fun <reified T : Any> Connection.querySingle(
  *
  * @param clazz The class to map the result to.
  * @param sql The SQL query to execute.
- * @param args Optional parameters to be substituted in the SQL query.
- * @return The query result single [T] instance or null if 0 results found.
+ * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the first item in each Pair.
+ * @return Returns a single result or `null` if no results are found. Throws an exception if more than one result is present.
  */
 fun <T : Any> Connection.querySingle(
     clazz: KClass<T>,
@@ -119,8 +198,8 @@ fun <T : Any> Connection.querySingle(
  * @param clazz The class to map the result to.
  * @param sql The SQL query to execute.
  * @param mapper Optional mapping function to map the [ResultSet] to the target class.
- * @param args Optional parameters to be substituted in the SQL query.
- * @return The query result single [T] instance or null if 0 results found.
+ * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the first item in each Pair.
+ * @return Returns a single result or `null` if no results are found. Throws an exception if more than one result is present.
  */
 fun <T : Any> Connection.querySingle(
     clazz: KClass<T>,
@@ -132,9 +211,28 @@ fun <T : Any> Connection.querySingle(
 /**
  * Execute a SQL statement and return the number of affected rows.
  *
+ * This function is typically used for non-query SQL statements, such as `INSERT`, `UPDATE`, or `DELETE`.
+ *
+ * **Example**:
+ * ```kotlin
+ * // Update the "active" status of a user
+ * val rowsAffected = connection.execute(
+ *     sql = "UPDATE users SET active = :active WHERE id = :id",
+ *     "active" to false,
+ *     "id" to 1
+ * )
+ *
+ * println("Rows affected: $rowsAffected")
+ * ```
+ *
+ * **Edge Cases**:
+ * - Returns `0` if no rows are affected by the statement.
+ * - Throws an exception if there's an issue with the database connection or query syntax.
+ *
  * @param sql The SQL statement to execute.
- * @param args Optional parameters to be substituted in the SQL statement.
- * @return The number of affected rows.
+ * @param args Optional key-value pairs representing named parameters to substitute into the statement.
+ * @return The number of rows affected by the execution of the SQL statement.
+ * @throws SQLException If there's a database error.
  */
 fun Connection.execute(
     sql: String,
@@ -145,7 +243,7 @@ fun Connection.execute(
 
 /**
  * Kapper API interface for executing SQL statements and queries.
- * Used in cases where the extension methods cannot be used or ar not preferred.
+ * Used in cases where the extension methods cannot be used or are not preferred.
  */
 interface Kapper {
     companion object {
@@ -159,7 +257,7 @@ interface Kapper {
      * @param clazz The class to map the results to.
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
-     * @param args Optional parameters to be substituted in the SQL query.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the Map keys.
      * @return The query result as a list of [T] instances.
      */
     fun <T : Any> query(
@@ -176,7 +274,7 @@ interface Kapper {
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
      * @param mapper Optional mapping function to map the [ResultSet] to the target class.
-     * @param args Optional parameters to be substituted in the SQL query.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the Map keys.
      * @return The query result as a list of [T] instances.
      */
     fun <T : Any> query(
@@ -193,7 +291,7 @@ interface Kapper {
      * @param clazz The class to map the results to.
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
-     * @param args Optional parameters to be substituted in the SQL query.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the HashMap keys.
      * @return The query result as a list of [T] instances.
      */
     fun <T : Any> query(
@@ -210,7 +308,7 @@ interface Kapper {
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
      * @param mapper Optional mapping function to map the [ResultSet] to the target class.
-     * @param args Optional parameters to be substituted in the SQL query.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the HashMap keys.
      * @return The query result as a list of [T] instances.
      */
     fun <T : Any> query(
@@ -222,13 +320,13 @@ interface Kapper {
     ): List<T> = query(clazz, connection, sql, mapper, args.toMap())
 
     /**
-     * Execute a SQL query and map the result to a single instance of the specified class, or null of no results found.
+     * Execute a SQL query and map the result to a single instance of the specified class, or null if no results are found.
      *
      * @param clazz The class to map the result to.
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
-     * @param args Optional parameters to be substituted in the SQL query.
-     * @return The query result single [T] instance or null if 0 results found.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the HashMap keys.
+     * @return Returns a single result or `null` if no results are found. Throws an exception if more than one result is present.
      */
     fun <T : Any> querySingle(
         clazz: Class<T>,
@@ -244,8 +342,8 @@ interface Kapper {
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
      * @param mapper Optional mapping function to map the [ResultSet] to the target class.
-     * @param args Optional parameters to be substituted in the SQL query.
-     * @return The query result single [T] instance or null if 0 results found.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the HashMap keys.
+     * @return Returns a single result or `null` if no results are found. Throws an exception if more than one result is present.
      */
     fun <T : Any> querySingle(
         clazz: Class<T>,
@@ -261,8 +359,8 @@ interface Kapper {
      * @param clazz The class to map the result to.
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
-     * @param args Optional parameters to be substituted in the SQL query.
-     * @return The query result single [T] instance or null if 0 results found.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the Map keys.
+     * @return Returns a single result or `null` if no results are found. Throws an exception if more than one result is present.
      */
     fun <T : Any> querySingle(
         clazz: Class<T>,
@@ -278,8 +376,8 @@ interface Kapper {
      * @param connection The SQL connection to use.
      * @param sql The SQL query to execute.
      * @param mapper Optional mapping function to map the [ResultSet] to the target class.
-     * @param args Optional parameters to be substituted in the SQL query.
-     * @return The query result single [T] instance or null if 0 results found.
+     * @param args Optional parameters to be substituted in the SQL query during execution. Parameter substitution is based on the Map keys.
+     * @return Returns a single result or `null` if no results are found. Throws an exception if more than one result is present.
      */
     fun <T : Any> querySingle(
         clazz: Class<T>,
@@ -294,7 +392,7 @@ interface Kapper {
      *
      * @param connection The SQL connection to use.
      * @param sql The SQL statement to execute.
-     * @param args Optional parameters to be substituted in the SQL statement.
+     * @param args Optional parameters to be substituted in the SQL statement. Parameter substitution is based on the HashMap keys.
      * @return The number of rows affected by the statement.
      */
     fun execute(
@@ -308,7 +406,7 @@ interface Kapper {
      *
      * @param connection The SQL connection to use.
      * @param sql The SQL statement to execute.
-     * @param args Optional parameters to be substituted in the SQL statement.
+     * @param args Optional parameters to be substituted in the SQL statement. Parameter substitution is based on the Map keys.
      * @return The number of rows affected by the statement.
      */
     fun execute(
