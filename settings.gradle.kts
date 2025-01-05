d97711425ffe7c58f8595629ca47d09bef9f1f2d
Dries Samyn <5557551+driessamyn@users.noreply.github.com>
2025-01-05 15:29:52 +0000

    Chore: Use releaser plugin to deploy to Maven Central and GH packags. Chore: Add Release action.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("com.gradle.enterprise") version "3.19"
+    id("com.gradle.develocity") version "3.19"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        publishAlways()
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
     }
 }
 
