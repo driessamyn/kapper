d4cd5fbe256443c9062337cf9ad027e84f12c5cc
Dries Samyn <5557551+driessamyn@users.noreply.github.com>
2025-10-11 16:57:00 +0100

    refactor: simplify code.

--- core/src/main/kotlin/net/samyn/kapper/internal/DbFlavourFunc.kt
+++ core/src/main/kotlin/net/samyn/kapper/internal/DbFlavourFunc.kt
@@ -6,18 +6,15 @@ import net.samyn.kapper.DbFlavour
 import java.sql.Connection
 
 fun Connection.getDbFlavour(): DbFlavour {
-    val productName = this.metaData.databaseProductName.lowercase()
-    return if (productName.contains("postgres") || productName.contains("enterprisedb")) {
-        DbFlavour.POSTGRESQL
-    } else if (productName.contains("mysql")) {
-        DbFlavour.MYSQL
-    } else if (productName.contains("sqlite")) {
-        DbFlavour.SQLITE
-    } else if (productName.contains("oracle")) {
-        DbFlavour.ORACLE
-    } else if (productName.contains("sql server") || productName.contains("mssql")) {
-        DbFlavour.MSSQLSERVER
-    } else {
-        DbFlavour.UNKNOWN
+    val productName = this.metaData.databaseProductName
+    return when {
+        productName.contains("postgres", ignoreCase = true) ||
+            productName.contains("enterprisedb", ignoreCase = true) -> DbFlavour.POSTGRESQL
+        productName.contains("mysql", ignoreCase = true) -> DbFlavour.MYSQL
+        productName.contains("sqlite", ignoreCase = true) -> DbFlavour.SQLITE
+        productName.contains("oracle", ignoreCase = true) -> DbFlavour.ORACLE
+        productName.contains("sql server", ignoreCase = true) ||
+            productName.contains("mssql", ignoreCase = true) -> DbFlavour.MSSQLSERVER
+        else -> DbFlavour.UNKNOWN
     }
 }
